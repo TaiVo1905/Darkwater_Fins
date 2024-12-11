9097cca3f516f74ab427c6c7b182768459811962
Vo Duc Tai <voductaitxqt123@gmail.com>
2024-12-12 03:33:43 +0700

    DF-11, DF-56 feat: add front end for a user log in

--- app/views/logInView.php
+++ app/views/logInView.php
@@ -16,15 +16,11 @@
                 <a href="Home" class="btn custom-btn-outline justify-content-start col-4">Back to home</a>
             </div>
         </div>
-        <div class="d-flex align-items-center mt-5">
+        <div class="d-flex align-items-center h-75 mt-3">
             <div class="col-6">
-                <div class="display-3 fw-bold text-white mx-auto">Welcome, Looks like you're new here!</div>
+                <div class="display-3 fw-bold text-white mx-auto">Welcome, We are glad to see you again!</div>
             </div>
             <form action="" class= "col-4 mx-auto shadow px-3 py-5 rounded-3 needs-validation" novalidate>
-                <div class="mb-3">
-                    <input type="text" class="form-control form-control custom-opacity-form" id="user_name" placeholder="Full name" required>
-                    <div class="invalid-feedback">Please provide your name</div>
-                </div>
                 <div class="mb-3">
                     <input type="email" class="form-control form-control custom-opacity-form" id="user_email" placeholder="Email" required>
                     <div class="invalid-feedback">Invalid email</div>
@@ -33,25 +29,11 @@
                     <input type="password" class="form-control form-control custom-opacity-form" id="user_password" placeholder="Password" required>
                     <div class="invalid-feedback">Password must be more than 8 characters</div>
                 </div>
-                <div class="mb-3">
-                    <input type="text" class="form-control form-control custom-opacity-form" id="user_confirm_password" placeholder="Password confirmation" required>
-                    <div class="invalid-feedback">Invalid password confirmation</div>
-                </div>
-                <div class="mb-3">
-                    <div class="row g-3">
-                        <div class="col-8">
-                            <input type="password" class="form-control form-control custom-opacity-form" id="user_authentication_code" placeholder="Authentication code" required>
-                            <div class="invalid-feedback">Invalid authentication code</div>
-                        </div>
-                        <div class="col-4 d-grid">
-                            <button type="button" class="btn btn-sm custom-btn-outline fw-bold mb-auto">Send code</button>
-                        </div>
-                    </div>
-                </div>
                 <div class="d-grid">
-                    <button type="submit" class="btn custom-btn-outline fw-bold">Sign up</button>
+                    <button type="submit" class="btn custom-btn-outline fw-bold">Sign in</button>
                 </div>
-                <h6 class="pt-3 text-center">Already a member? <a href="LogIn" class="custom-color">Sign in</a></h6>
+                <h6 class="pt-4 text-center"><a href="ForgotPassword" class="custom-color">Forgot password?</a></h6>
+                <h6 class="pt-3 text-center">Not a member? <a href="Register" class="custom-color">Sign up</a></h6>
             </form>
         </div>
     </div>
