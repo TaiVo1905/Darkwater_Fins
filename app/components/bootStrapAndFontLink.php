5735795d2f6fdfa6c02af82a13e7c17c89f1435c
MaiTram01 <tramh7879@gmail.com>
2024-12-11 14:30:55 +0700

    DF-35, DF-59 feat: add a hot fishes component

--- app/components/bootStrapAndFontLink.php
+++ app/components/bootStrapAndFontLink.php
@@ -1,5 +1,4 @@
 <link href="https://fonts.googleapis.com/css2?family=Kaisei+Tokumin:wght@400;500;700&display=swap" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
